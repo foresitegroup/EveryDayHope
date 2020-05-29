0723f71c130a2f383b7dc060e4e7b391f995dd42
marklippert <lippert@gmail.com>
2020-05-29 17:17:54 -0500

    Layout changes and image replacement

--- clean-water.php
+++ clean-water.php
@@ -54,9 +54,9 @@ function HeaderContent() { ?>
 <script type="text/javascript">
   $(document).ready(function() {
     $("#details-header .button").click(function(){
-      $("#details-header .button, #details .text, #details .images").removeClass("current");
+      $("#details-header .button, #details .tab-content").removeClass("current");
       $(this).add("#"+$(this).data("tab")).addClass("current");
-      $(this).add("."+$(this).data("tab")).addClass("current");
+      // $(this).add("."+$(this).data("tab")).addClass("current");
     });
   });
 </script>
@@ -71,58 +71,64 @@ function HeaderContent() { ?>
 </div>
 
 <div id="details">
-  <div class="site-width">
-    <div class="text current" id="tab1">
-      <h2>Every Day Hope Water Stations</h2>
+  <div id="tab1" class="tab-content current">
+    <div class="site-width">
+      <div class="text">
+        <h2>Every Day Hope Water Stations</h2>
 
-      Clean running water is not available to all of the Dominican Republic's population. Rural communities face the most challenges, as many people obtain their water from sources that are unprotected and contaminated. Others must use contaminated canal water or rain water collected from contaminated rooftop systems which are often without water during the dry season. Utilizing contaminated water for domestic use, leads to disease. Disease leads to loss of time, money, work and quality of life.<br>
-      <br>
+        Clean running water is not available to all of the Dominican Republic's population. Rural communities face the most challenges, as many people obtain their water from sources that are unprotected and contaminated. Others must use contaminated canal water or rain water collected from contaminated rooftop systems which are often without water during the dry season. Utilizing contaminated water for domestic use, leads to disease. Disease leads to loss of time, money, work and quality of life.<br>
+        <br>
 
-      Every Day Hope believes that clean water changes lives. To bring hope to this situation, EDH is collaborating with international partners, local organizations and communities to drill safe wells and build water stations. The water stations implements either a hand pump or electric pump depending on the community's demographics and daily water usage.<br>
-      <br>
+        Every Day Hope believes that clean water changes lives. To bring hope to this situation, EDH is collaborating with international partners, local organizations and communities to drill safe wells and build water stations. The water stations implements either a hand pump or electric pump depending on the community's demographics and daily water usage.<br>
+        <br>
 
-      In addition to installing the pumps, Every Day Hope establishes community cooperatives. The cooperatives consist of a council of people who live in the community and oversee, direct, and manage the water stations in their area. The cooperatives are also educated on proper usage, regulations, and the maintenance of the pumps. These cooperatives help raise funds and collect a minimal monthly usage fee from the community members in order to maintain the wells for generations. This promotes sustainability and a positive approach to being the change. But they need our help to begin.<br>
-      <br>
+        In addition to installing the pumps, Every Day Hope establishes community cooperatives. The cooperatives consist of a council of people who live in the community and oversee, direct, and manage the water stations in their area. The cooperatives are also educated on proper usage, regulations, and the maintenance of the pumps. These cooperatives help raise funds and collect a minimal monthly usage fee from the community members in order to maintain the wells for generations. This promotes sustainability and a positive approach to being the change. But they need our help to begin.<br>
+        <br>
 
-      There are two types of Clean Water Stations that bring clean (non potable water) to communities. The relative costs are as follows:<br>
-      <br>
+        There are two types of Clean Water Stations that bring clean (non potable water) to communities. The relative costs are as follows:<br>
+        <br>
 
-      <div class="pumps">
-        <div>Well Drilling, Reserve Tank and Electric Pump</div>
-        $6,000-$9,000
-      </div>
+        <div class="pumps">
+          <div>Well Drilling, Reserve Tanks and Automated Electric Pump with Distribution Centers</div>
+          $10,000-$25,000
+        </div>
 
-      <div class="pumps right">
-        <div>Well Drilled, Hand Pump Station</div>
-        $3,000-$5,000
-      </div>
+        <div class="pumps right">
+          <div>Well Drilling and Hand Pump Station</div>
+          $3,000-$5,000
+        </div>
 
-      <a href="#giving" class="button togiving">Give Now</a>
-    </div>
+        <a href="#giving" class="button togiving">Give Now</a>
+      </div> <!-- /.text -->
+    </div> <!-- /.site-width -->
 
-    <div class="text" id="tab2">
-      <h2>Clean Water Also Involves Education</h2>
+    <div class="images">
+      <div style="background-image: url(images/safewater-details1.jpg);"></div>
+      <div style="background-image: url(images/cleanwater-details2.jpg);"></div>
+    </div> <!-- /.images -->
+  </div> <!-- /#tab1 -->
+  
+  <div id="tab2" class="tab-content">
+    <div class="site-width">
+      <div class="text">
+        <h2>Clean Water Also Involves Education</h2>
 
-      Access to clean water can protect and save lives, just because it's there. Many people are without clean water for daily living in the Dominican Republic as there are not even water lines to carry usable water to their communities. Every Day Hope partners with local communities to establish wells that provide safe daily use water with the purpose of seeing communities thrive.<br>
-      <br>
+        Access to clean water can protect and save lives, just because it's there. Many people are without clean water for daily living in the Dominican Republic as there are not even water lines to carry usable water to their communities. Every Day Hope partners with local communities to establish wells that provide safe daily use water with the purpose of seeing communities thrive.<br>
+        <br>
 
-      We believe water is one of the ways to break the cycle of poverty, to protect and save lives, and to bring hope. Provision of clean water alone however, will not improve health as there is an educational and awareness component that must be addressed. Community wide "Health and Hygiene Campaigns" are a vital part of a water supply project. Knowledge and behavior change as well as community involvement and commitment are crucial for transformation.<br>
-      <br>
+        We believe water is one of the ways to break the cycle of poverty, to protect and save lives, and to bring hope. Provision of clean water alone however, will not improve health as there is an educational and awareness component that must be addressed. Community wide "Health and Hygiene Campaigns" are a vital part of a water supply project. Knowledge and behavior change as well as community involvement and commitment are crucial for transformation.<br>
+        <br>
 
-      <a href="#giving" class="button togiving">Give Now</a>
-    </div>
-  </div>
+        <a href="#giving" class="button togiving">Give Now</a>
+      </div> <!-- /.text -->
+    </div> <!-- /.site-width -->
 
-  <div class="tab1 images cf current">
-    <div class="image1" style="background-image: url(images/safewater-details1.jpg);"></div>
-    <div class="image2" style="background-image: url(images/cleanwater-details2.jpg);"></div>
-  </div>
-
-  <div class="tab2 images cf">
-    <div class="image1" style="background-image: url(images/cleanwater-tab2-details1.jpg);"></div>
-    <div class="image2" style="background-image: url(images/cleanwater-tab2-details2.jpg);"></div>
-  </div>
-</div>
+    <div class="images">
+      <div style="background-image: url(images/cleanwater-tab2-details1.jpg);"></div>
+      <div style="background-image: url(images/cleanwater-tab2-details2.jpg);"></div>
+    </div> <!-- /.images -->
+  </div> <!-- /#tab2 -->
+</div> <!-- /#details -->
 
 <div id="giving-header">
   <div class="right">

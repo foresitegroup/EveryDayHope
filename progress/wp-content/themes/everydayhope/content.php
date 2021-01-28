54bc91c49f8fbb03118b2739cc1c8974a9a99730
marklippert <lippert@gmail.com>
2021-01-28 10:44:25 -0600

    Remove categories

--- progress/wp-content/themes/everydayhope/content.php
+++ progress/wp-content/themes/everydayhope/content.php
@@ -5,13 +5,7 @@ if (!is_single()) :
     <div class="index-post-image" style="<?php echo (wp_get_attachment_url(get_post_thumbnail_id()) != "") ? "background-image: url(" . wp_get_attachment_url(get_post_thumbnail_id()) . ")" : "padding-top: 0; margin-bottom: 0;"; ?>"></div>
     
     <div class="index-post-text">
-      <h3>
-      <?php
-      echo get_the_date('F j, Y') . " &bullet; ";
-
-      echo strip_tags(get_the_category_list(', '));
-      ?>
-      </h3>
+      <h3><?php echo get_the_date('F j, Y'); ?></h3>
 
       <?php the_title('<h2>', '</h2>'); ?>
 
